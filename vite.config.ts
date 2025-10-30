fba8e3b6ff3ec6e31eea278c20b336ac40ad6702
Abdullah Darwech <darwechabdullh@gmail.com>
2025-10-30 09:20:15 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚠️ هذا السطر هو المفتاح لحل المشكلة
 export default defineConfig({
   plugins: [react()],
-  base: '/Cardly/', // اكتب اسم الريبو بالضبط كما هو في GitHub
+  base: '/Cardly/',  // اسم الريبو بالضبط
 })
